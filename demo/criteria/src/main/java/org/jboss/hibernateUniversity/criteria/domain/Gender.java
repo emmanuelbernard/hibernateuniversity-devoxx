2c2e44dc8d4df25063b4e3575cdea53eb64e4652
Emmanuel Bernard <emmanuel@hibernate.org>
2010-11-09 22:20:11 +0100

    Improve domain model to support more queries

---
+++ demo/criteria/src/main/java/org/jboss/hibernateUniversity/criteria/domain/Gender.java
@@ -0,0 +1,10 @@
+package org.jboss.hibernateUniversity.criteria.domain;
+
+/**
+ * @author Emmanuel Bernard
+ */
+public enum Gender {
+	MALE,
+	FEMALE,
+	NOT_SURE
+}
--- demo/criteria/src/main/java/org/jboss/hibernateUniversity/criteria/domain/User.java
+++ demo/criteria/src/main/java/org/jboss/hibernateUniversity/criteria/domain/User.java
@@ -1,12 +1,17 @@
 package org.jboss.hibernateUniversity.criteria.domain;
 
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 /**
  * @author Emmanuel Bernard
@@ -14,9 +19,13 @@ import javax.persistence.OneToMany;
 @Entity
 public class User {
 
-	public User(String firstName, String lastName) {
+	public User(String firstName, String lastName, Date birthDate, Gender gender, int credits, String username, String password) {
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.birthDate = birthDate;
+		this.gender = gender;
+		this.credits = credits;
+		this.login = new Login(username, password);
 	}
 
 	@Id @GeneratedValue
@@ -37,4 +46,21 @@ public class User {
 	public void setAddresses(Set<Address> addresses) {  this.addresses = addresses; }
 	private Set<Address> addresses = new HashSet<Address>();
 
+	@Temporal(TemporalType.DATE)
+	public Date getBirthDate() { return birthDate; }
+	public void setBirthDate(Date birthDate) {  this.birthDate = birthDate; }
+	private Date birthDate;
+
+	@Enumerated(EnumType.STRING)
+	public Gender getGender() { return gender; }
+	public void setGender(Gender gender) {  this.gender = gender; }
+	private Gender gender;
+
+	public int getCredits() { return credits; }
+	public void setCredits(int credits) {  this.credits = credits; }
+	private int credits;
+
+	public Login getLogin() { return login; }
+	public void setLogin(Login login) {  this.login = login; }
+	private Login login;
 }
