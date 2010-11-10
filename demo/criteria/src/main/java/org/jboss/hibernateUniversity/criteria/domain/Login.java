64b6d68ab4a5726f6093c3ae3370466041826c9d
Emmanuel Bernard <emmanuel@hibernate.org>
2010-11-10 18:29:29 +0100

    Add no-arg constructor to login entity

--- demo/criteria/src/main/java/org/jboss/hibernateUniversity/criteria/domain/Login.java
+++ demo/criteria/src/main/java/org/jboss/hibernateUniversity/criteria/domain/Login.java
@@ -7,6 +7,8 @@ import javax.persistence.Embeddable;
  */
 @Embeddable
 public class Login {
+	public Login() {}
+	
 	public Login(String username, String password) {
 		this.username = username;
 		this.password = password;
